13a0fcc55b20e7f17dd8d0051a2688f7fd196a7a
Jhonatan Fernandes <jhonnfernandes90@gmail.com>
2021-07-18 12:23:11 -0300

    Adicionando scriptjs

---
+++ scriptjs/script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit' , (e ) => {
+e.preventDefault( ); 
+
+let nome = document.getElementById('nome').value;
+let email = document.getElementById('email').value;
+let data = {
+    nome,
+    email,
+}
+let convertData = JSON.stringify(data);
+localStorage.setItem('lead', convertData)
+
+let content = document.getElementById('content')
+
+let carregando = '<p>Carregando...</p>'
+
+let pronto = '<p>Pronto.</p>'
+
+content.innerHTML = carregando
+
+setTimeout(() => {
+    content.innerHTML = pronto
+}, 1000)
+
+})
